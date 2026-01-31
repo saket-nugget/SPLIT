d1a763449ce3992be51a4846d46a937fc04a7a73
saket-nugget <saketramavedula@gmail.com>
2026-01-31 17:21:28 +0530

    fix(mobile): force app shell height to 100dvh

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import SplitScreen from './components/Layout/SplitScreen';
 function App() {
     return (
         <BillProvider>
-            <SplitScreen />
+            <div className="h-[100dvh] w-screen overflow-hidden bg-background-dark text-white">
+                <SplitScreen />
+            </div>
         </BillProvider>
     );
 }
